2ddef6259b6466d83806a5d545dca6df00faa0e1
Saidolim Djuraev <saidolim@gmail.com>
2022-08-27 14:30:57 +0500

    Dogs and Customers changed

--- namuna-2/locations-select-query.sql
+++ namuna-2/locations-select-query.sql
@@ -1,8 +1,40 @@
+-- Avgust oyi va undan keyingi ma'lumotlarni olish
 select *
 from public.locations
 where reg_date > '2022-08-01';
 
+-- Analiz qilamiz
+-- Avgust oyi va undan keyingi ma'lumotlarni olish
+-- 2 ta jadval ishlatildi. locations_y2022m08 va locations_y2023
 explain analyse
 select *
 from public.locations
 where reg_date > '2022-08-01';
+
+-- Iyun va Ijul oylaridagi ma'lumotlarni olamiz
+select *
+from locations
+where reg_date > '2022-07-01'
+  and reg_date < '2022-08-01';
+
+-- Iyun va Ijul oylaridagi ma'lumotlarni analiz qilamiz
+-- 2 ta jadval ishlatildi. locations_y2022m06 va locations_y2022m06
+explain analyse
+select *
+from locations
+where reg_date > '2022-06-01'
+  and reg_date < '2022-08-01';
+
+-- Bitta oy ichida ma'lumot olib ko`ramiz
+select *
+from locations
+where reg_date > '2022-08-01'
+  and reg_date < '2022-08-20';
+
+-- Bitta oy ichida ma'lumotlarni analiz qilamiz
+-- 1 ta jadval ishlatildi. locations_y2022m08
+explain analyse
+select *
+from locations
+where reg_date > '2022-08-01'
+  and reg_date < '2022-08-20';
